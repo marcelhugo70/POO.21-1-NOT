9eac3c36ddea4101e1d3645c4e8c540b3cb59195
Marcel <marcel@furb.br>
2021-03-17 21:10:49 -0300

    Lista 2 - Exe08 início

---
+++ Lista2-Exe08/src/PassageirosHora.java
@@ -0,0 +1,15 @@
+
+public class PassageirosHora {
+	
+	private int[][][] matriz = new int[12][30][24]; //12 meses x 30 dias x 24 horas
+	// índices: 0 a 11; 0 a 29; 0 a 23
+	
+	public void adiciona(int dia, int mes, int hora) {
+		//this.matriz[mes-1][dia-1][hora] = this.matriz[mes-1][dia-1][hora] + 1;
+		this.matriz[mes-1][dia-1][hora]++;
+	}
+	
+	public int quantosPassageiros(int dia, int mes) {
+		// to do
+	}
+}
